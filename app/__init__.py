77cab38c9463ad9bca6d7d8ebb2ba2cfdd463881
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-08-18 02:34:09 +0200

    starting update thread

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from app.utils.update_thread import start_update_thread
 
 app = Flask(__name__)
 CORS(app)
+start_update_thread()
 
 from app import views
 from app import api
